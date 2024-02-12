3c097f60cf7680f544762a7da77c6d2eeb1a6c04
TG-Oikawa <to2319@tokyo-gas.co.jp>
2024-02-12 19:19:04 +0900

    admin

--- app/Admin/Controllers/ProductController.php
+++ app/Admin/Controllers/ProductController.php
@@ -2,21 +2,21 @@
 
 namespace App\Admin\Controllers;
 
+use App\Models\Product;
 use App\Models\Category;
-use App\Models\MajorCategory;
 use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
 
-class CategoryController extends AdminController
+class ProductController extends AdminController
 {
     /**
      * Title for current resource.
      *
      * @var string
      */
-    protected $title = 'Category';
+    protected $title = 'Product';
 
     /**
      * Make a grid builder.
@@ -25,19 +25,22 @@ class CategoryController extends AdminController
      */
     protected function grid()
     {
-        $grid = new Grid(new Category());
+        $grid = new Grid(new Product());
 
         $grid->column('id', __('Id'))->sortable();
         $grid->column('name', __('Name'));
         $grid->column('description', __('Description'));
-        $grid->column('major_category_id', __('Major category name'))->editable('select', MajorCategory::all()->pluck('name','id'));
+        $grid->column('price', __('Price'))->sortable();
+        $grid->column('category.name', __('Category Name'));
+        $grid->column('image', __('Image'))->image();
         $grid->column('created_at', __('Created at'))->sortable();
         $grid->column('updated_at', __('Updated at'))->sortable();
 
         $grid->filter(function($filter) {
-            $filter->like('name', 'カテゴリー名');
-            $filter->like('major_category_id', '親カテゴリー名')->multipleSelect(MajorCategory::all()->pluck('name', 'id'));
-            $filter->between('created_at', '登録日')->datetime();
+            $filter->like('name', '商品名');
+            $filter->like('description', '商品説明');
+            $filter->between('price', '金額');
+            $filter->in('category_id', 'カテゴリー')->multipleSelect(Category::all()->pluck('name', 'id'));
         });
 
         return $grid;
@@ -51,12 +54,14 @@ class CategoryController extends AdminController
      */
     protected function detail($id)
     {
-        $show = new Show(Category::findOrFail($id));
+        $show = new Show(Product::findOrFail($id));
 
         $show->field('id', __('Id'));
         $show->field('name', __('Name'));
         $show->field('description', __('Description'));
-        $show->field('major_category.name', __('Major category name'));
+        $show->field('price', __('Price'));
+        $show->field('category.name', __('Category Name'));
+        $show->field('image', __('Image'))->image();
         $show->field('created_at', __('Created at'));
         $show->field('updated_at', __('Updated at'));
 
@@ -70,11 +75,13 @@ class CategoryController extends AdminController
      */
     protected function form()
     {
-        $form = new Form(new Category());
+        $form = new Form(new Product());
 
         $form->text('name', __('Name'));
         $form->textarea('description', __('Description'));
-        $form->text('major_category_id', __('Major category name'))->options(MajorCategory::all()->pluck('name', 'id'));
+        $form->number('price', __('Price'));
+        $form->select('category_id', __('Category Name'))->options(Category::all()->pluck('name', 'id'));
+        $form->image('image', __('Image'));
 
         return $form;
     }
--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Routing\Router;
 use App\Admin\Controllers\CategoryController;
+use App\Admin\Controllers\ProductController;
+use App\Admin\Controllers\MajorCategoryController;
+use App\Admin\Controllers\UserController;
 
 Admin::routes();
 
@@ -14,5 +17,8 @@ Route::group([
 
     $router->get('/', 'HomeController@index')->name('home');
     $router->resource('categories', CategoryController::class);
+    $router->resource('products', ProductController::class);
+    $router->resource('major-categories', MajorCategoryController::class);
+    $router->resource('users', UserController::class);
 
 });
